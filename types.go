98c99cc4a0cfee64c447172623dfdd1eaa979c50
Oussama Sethoum <os.sethoum@gmail.com>
2024-02-24 18:33:54 +0000

    remove unnecessary config

--- types.go
+++ types.go
@@ -48,36 +48,6 @@ type Config struct {
 	FormTag        bool
 }
 
-type Action = string
-
-const (
-	AllAction       Action = "AllAction"
-	AggregateAction Action = "AggregateAction"
-	QueryAction     Action = "QueryAction"
-	CreateAction    Action = "CreateAction"
-	UpdateAction    Action = "UpdateAction"
-	DeleteAction    Action = "DeleteAction"
-)
-
-type MiddlewareRoutes struct {
-	Routes  []string `json:"routes,omitempty"`
-	Actions []Action `json:"actions,omitempty"`
-}
-
-type MiddlewarePosition bool
-
-const (
-	BeforeHandler MiddlewarePosition = true
-	AfterHandler  MiddlewarePosition = false
-)
-
-type Middleware struct {
-	Import   string             `json:"import,omitempty"`
-	Name     string             `json:"name,omitempty"`
-	Routes   MiddlewareRoutes   `json:"routes,omitempty"`
-	Position MiddlewarePosition `json:"position,omitempty"`
-}
-
 type comparable interface {
 	~string | ~int | ~float32 | ~uint
 }
